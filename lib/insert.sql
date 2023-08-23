439c36192ee2d3269c43bb7b61a189ff6dca4cbe
karanja03 <wambuik03@gmail.com>
2023-08-23 22:07:45 +0300

    Complete lab

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(1,"Mr.Chocolate", 45, "M", "white", "choleric", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(2, "Rowdy", 23, "M", "white", "sanguine", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(3, "Tabitha", 6, "F", "brown", "choleric", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(4, "Sergeant Brown", 25, "F", "brown", "sanguine", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(5, "Melissa", 56, "F", "brown", "phlegmatic", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(6,"Grinch", 67, "M", "grey", "sanguine", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(7, "Wendy", 13, "F", "grey", "phlegmatic", TRUE);
+INSERT INTO bears(id, name, age, sex, color, temperament, alive) VALUES(8, NULL, 45, "M", "grey", "phlegmatic", TRUE);
